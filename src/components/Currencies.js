239582b70e3ad0c9624b9af2063fd17454c96830
teodorpavlov <amzon906@gmail.com>
2020-08-08 19:09:32 +0300

    work

--- src/components/Currencies.js
+++ src/components/Currencies.js
@@ -5,18 +5,27 @@ export default function Currencies({ rates }) {
   let min = 1;
   let counter = 0;
 
-  for (const rate in rates) {
-    rates[rate] = rates[rate].toFixed(4);
-    rates[rate] = Number(rates[rate]);
+  // Change background color of the rates
+  function changeBackground() {
+    let rateBackground = document.getElementsByClassName("rates");
+    Array.from(rateBackground).forEach((el) => {
+      min % 2 !== 0
+        ? el.setAttribute("style", "background-color:	#37a08b")
+        : el.setAttribute("style", "background-color: #e85456");
+    });
   }
 
-  let [state, setState] = useState(rates);
+  for (const rate in rates) {
+    rates[rate] = parseFloat(rates[rate].toFixed(4));
+  }
 
+  let [, setState] = useState(rates);
+
+  // Change rates depending on the minute
   function changeRates() {
     for (const rate in rates) {
       min % 2 === 0 ? (rates[rate] -= 0.0001) : (rates[rate] += 0.0001);
-      rates[rate] = rates[rate].toFixed(4);
-      rates[rate] = Number(rates[rate]);
+      rates[rate] = parseFloat(rates[rate].toFixed(4));
       if (counter === 12) {
         min++;
         counter = 0;
@@ -26,17 +35,15 @@ export default function Currencies({ rates }) {
     if (min === 5) {
       clearInterval(window.interval);
     }
-    setState((state = rates)); // <--------- It shouldn't be done this way. It works in the console. Trying to find a solution...
+    setState({ rates });
+    changeBackground();
   }
 
   useEffect(() => {
     window.interval = setInterval(() => {
       changeRates();
       counter++;
-      console.log(state);
-      console.log(min);
-      console.log(counter);
-    }, 1000);
+    }, 5000);
   }, []);
 
   return (
@@ -45,50 +52,22 @@ export default function Currencies({ rates }) {
         <span className="currency EURUSD">
           <h4>EURUSD</h4>
           <p>EURO / U.S. DOLLAR</p>
-          <div
-            className="rates"
-            style={
-              min % 2 === 0 ? { background: "red" } : { background: "green" }
-            }
-          >
-            {state.USD}
-          </div>
+          <div className="rates">{rates.USD}</div>
         </span>
         <span className="currency EURAUD">
           <h4>EURAUD</h4>
           <p>EURO / AUSTRALIAN DOLLAR</p>
-          <div
-            className="rates"
-            style={
-              min % 2 === 0 ? { background: "red" } : { background: "green" }
-            }
-          >
-            {state.AUD}
-          </div>
+          <div className="rates">{rates.AUD}</div>
         </span>
         <span className="currency EURCAD">
           <h4>EURCAD</h4>
           <p>EURO / CANADIAN DOLLAR </p>
-          <div
-            className="rates"
-            style={
-              min % 2 === 0 ? { background: "red" } : { background: "green" }
-            }
-          >
-            {state.CAD}
-          </div>
+          <div className="rates">{rates.CAD}</div>
         </span>
         <span className="currency EURBGN">
           <h4>EURBGN</h4>
           <p>EURO / BULGARIAN LEV</p>
-          <div
-            className="rates"
-            style={
-              min % 2 === 0 ? { background: "red" } : { background: "green" }
-            }
-          >
-            {state.BGN}
-          </div>
+          <div className="rates">{rates.BGN}</div>
         </span>
       </div>
     </div>
